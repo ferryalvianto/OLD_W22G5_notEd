614ee79fc1239a2570a5579a941b4192f0eaccc7
ferrytjahjadi <ferryalvianto@gmail.com>
2022-03-07 17:16:44 -0800

    added keep me signed in option

--- app/src/main/java/com/example/noted/LoginActivity.java
+++ app/src/main/java/com/example/noted/LoginActivity.java
@@ -3,9 +3,12 @@ package com.example.noted;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +19,8 @@ public class LoginActivity extends AppCompatActivity {
     TextView signup;
     Button login;
     DBHelper DB;
+    CheckBox checkBox;
+    boolean isChecked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +31,55 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.inputPassword);
         login = findViewById(R.id.btnLogin);
         signup = findViewById(R.id.txtViewSignup);
-
+        checkBox = findViewById(R.id.cbSignIn);
+        isChecked = false;
         DB = new DBHelper(this);
 
-        login.setOnClickListener(view -> {
-            String user = username.getText().toString();
-            String pass = password.getText().toString();
-
-            if (user.equals("") || pass.equals("")) {
-                Toast.makeText(LoginActivity.this, "Input cannot be empty. Please input username or password correctly.", Toast.LENGTH_SHORT).show();
-            } else {
-                Boolean checkuserpass = DB.CheckUsernamePassword(user, pass);
-                if (checkuserpass == true) {
-                    Toast.makeText(LoginActivity.this, "Successfully logged in.", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(LoginActivity.this, "Invalid credentials.", Toast.LENGTH_SHORT).show();
-                }
-            }
+        //store the state of checkbox in shared preferences
+        checkBox.setOnCheckedChangeListener((compoundButton, b) -> {
+            SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("isChecked", b);
+            editor.commit();
         });
 
-        signup.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+        //get the state of checkbox
+        SharedPreferences settings1 = getSharedPreferences("PREFS_NAME", 0);
+        isChecked = settings1.getBoolean("isChecked", false);
+
+        if (isChecked) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
-        });
+        } else {
+            login.setOnClickListener(view -> {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if (user.equals("") || pass.equals("")) {
+                    Toast.makeText(LoginActivity.this, "Input cannot be empty. Please input username or password correctly.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkuser = DB.CheckUsername(user);
+                    if (checkuser == true) {
+                        Boolean checkuserpass = DB.CheckUsernamePassword(user, pass);
+                        if (checkuserpass == true) {
+                            Toast.makeText(LoginActivity.this, "Successfully logged in.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Invalid credentials.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Username does not exist. Please create an account.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            signup.setOnClickListener(view -> {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            });
+        }
+
+
     }
 }
--- app/src/main/java/com/example/noted/MainActivity.java
+++ app/src/main/java/com/example/noted/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.noted;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.btnLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("isChecked", false);
+                editor.commit();
+
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
